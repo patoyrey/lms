4fcc23f7acdbda1713a1d382b5fc156fc7542377
Angel Kisha Arcenal <angelkishaarcenal10@gmail.com>
2024-02-12 10:41:20 +0800

    structuring directory

--- client/src/AppRouter.tsx
+++ client/src/AppRouter.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SignIn from "./signIn"
+import SignIn from "./views/auth/signIn"
 
 export const AppRouter: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='signin' element={<SignIn />}/>
+                <Route path='signin' element={<SignIn />} />
             </Routes>
         </BrowserRouter>
     )
