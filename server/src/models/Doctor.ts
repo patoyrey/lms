0a1e0fe71e8fc3c4d32d905a871d1e3279dc4ede
arvel <franciscoarvel123@gmail.com>
2024-02-16 09:52:53 +0800

    creating a schemas for doctor table and patientstest table

---
+++ server/src/models/Doctor.ts
@@ -0,0 +1,39 @@
+import { Gender } from "../enums/Genders";
+
+export class Doctor {
+  doc_id: string;
+  doc_fname: string;
+  doc_lname: string;
+  doc_mname: string;
+  doc_gender: Gender;
+  doc_specialization: string;
+  doc_prc_no: string;
+  doc_philhealth_no: string;
+  doc_tin_no: string;
+  doc_room_no: string;
+  doc_schedule: string;
+  doc_phone: string;
+  doc_price: number;
+  doc_created_at: string;
+  doc_udpated_at: string;
+  user_id: string;
+
+  constructor(init: Doctor) {
+    this.doc_id = init.doc_id;
+    this.doc_fname = init.doc_fname;
+    this.doc_lname = init.doc_lname;
+    this.doc_mname = init.doc_mname;
+    this.doc_gender = init.doc_gender;
+    this.doc_specialization = init.doc_specialization;
+    this.doc_prc_no = init.doc_prc_no;
+    this.doc_philhealth_no = init.doc_philhealth_no;
+    this.doc_tin_no = init.doc_tin_no;
+    this.doc_room_no = init.doc_room_no;
+    this.doc_schedule = init.doc_schedule;
+    this.doc_phone = init.doc_phone;
+    this.doc_price = init.doc_price;
+    this.doc_created_at = init.doc_created_at;
+    this.doc_udpated_at = init.doc_udpated_at;
+    this.user_id = init.user_id;
+  }
+}
--- server/src/models/dummyData/User.ts
+++ server/src/models/dummyData/User.ts
@@ -2,13 +2,11 @@ import { LoginStatus } from "../../enums/LoginStatus";
 import { UserType } from "../../enums/UserType";
 import { User } from "../User";
 
-
 export const dummyUser = new User({
-    user_id: "userId",
-    email: "email",
-    password: "password",
-    created_at: 'createdAt',
-    status: LoginStatus.Login,
-    userType: UserType.Admin,
-    updated_at: 'updated_at'
-})
+  user_id: "userId",
+  email: "email",
+  password: "password",
+  created_at: "createdAt",
+  status: LoginStatus.Login,
+  userType: UserType.Admin,
+});
