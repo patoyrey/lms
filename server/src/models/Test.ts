d520cf164e89391bd514ba42420ec5b30d6fcf07
vanmartinx <karlfred.martinez@gmail.com>
2024-02-19 14:27:31 +0800

    API for Nurse and Testfield

--- server/src/models/Test.ts
+++ server/src/models/Test.ts
@@ -11,7 +11,7 @@ export class Test {
     this.test_name = init.test_name;
   }
   public async add(): Promise<UserResponse> {
-    this.test_id = uuid4();
+    this.test_id = uuid4()
     const query = `insert into test SET ?`;
     console.log("query: ", query);
     await conn.query(query, [this], (err: any) => {
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,8 +2,15 @@ import express from "express";
 import { addUserHandler } from "./handlers/addUserHandler";
 import { addFieldsHandler } from "./handlers/addFieldsHandler";
 import { addTestHandler } from "./handlers/addTestHandler";
+import { addNurseHandler } from "./handlers/addNurseHandler";
+import { addTestFieldsHandler } from "./handlers/addTestFieldsHandler";
 export const routes = express.Router();
 
 routes.post("/add-user", addUserHandler);
 routes.post("/add-fields", addFieldsHandler);
 routes.post("/add-test", addTestHandler);
+routes.post("/add-nurse", addNurseHandler);
+routes.post("/add-testfields", addTestFieldsHandler);
+
+
+
